0b0a4fd231e1c21d1afb898dbc98f3a4fd0aa4a0
Kam Leung <kam_leung@yahoo.com>
2020-06-30 09:46:13 -0400

    Fixed compile warnings

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react'
-import CartItem from './CartItem'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function CartTotals({value}) {
